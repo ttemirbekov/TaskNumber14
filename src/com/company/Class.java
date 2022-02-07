249c61a245714c31dad2e49cdc17002e88cc311f
turatbek <temirbekov.tt@gmail.com>
2022-02-07 20:30:43 +0600

    Sorry for late

---
+++ src/com/company/Class.java
@@ -0,0 +1,54 @@
+package com.company;
+
+public class Class {
+
+    private int number;
+    private String word;
+    private int[] array;
+
+    public Class() {
+
+    }
+
+    public Class(int number, String word, int[] array) {
+        this.number = number;
+        this.word = word;
+        this.array = array;
+        System.out.print(number+", "+ word+", ");
+        for (int a:array){
+
+            System.out.print(a+" ");
+        }
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public int[] getArray() {
+        return array;
+    }
+
+    public void setArray(int[] array) {
+        this.array = array;
+
+
+    }
+
+
+
+    }
+
+
---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Class class1 = new Class(24, "soz", new int[]{1, 2, 5, 57, 8});
+    }
+}
